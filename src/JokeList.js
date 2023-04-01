6991eebc4b95e06c7d71a2e204730bbd677953ef
Curt <alpinecurt@gmail.com>
2023-03-31 17:04:07 -0700

    Refactoring complete

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,73 +1,83 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import axios from "axios";
 import Joke from "./Joke";
 import "./JokeList.css";
 
-function JokeList({ numJokesToGet = 10 }) {
-  const [jokes, setJokes] = useState([]);
-
-  /* get jokes if there are no jokes */
-
-  useEffect(function() {
-    async function getJokes() {
-      let j = [...jokes];
-      let seenJokes = new Set();
-      try {
-        while (j.length < numJokesToGet) {
-          let res = await axios.get("https://icanhazdadjoke.com", {
-            headers: { Accept: "application/json" }
-          });
-          let { status, ...jokeObj } = res.data;
-  
-          if (!seenJokes.has(jokeObj.id)) {
-            seenJokes.add(jokeObj.id);
-            j.push({ ...jokeObj, votes: 0 });
-          } else {
-            console.error("duplicate found!");
-          }
+class JokeList extends React.Component {
+    static defaultProps = {numJokesToGet: 10}
+    constructor(props) {
+        super(props);
+        this.state = {
+            jokes: []
         }
-        setJokes(j);
-      } catch (e) {
-        console.log(e);
-      }
+        this.getJokes();
+        this.generateNewJokes = this.generateNewJokes.bind(this);
+        this.getJokes = this.getJokes.bind(this);
+        this.vote = this.vote.bind(this);
     }
 
-    if (jokes.length === 0) getJokes();
-  }, [jokes, numJokesToGet]);
+    async getJokes() {
+        let j = [...this.state.jokes];
+        let seenJokes = new Set();
+        const {numJokesToGet} = this.props;
+        try {
+            while (j.length < numJokesToGet) {
+                let res = await axios.get("https://icanhazdadjoke.com", {
+                    headers: { Accept: "application/json" }
+                });
+                //debugger;
+                let { status, ...jokeObj } = res.data;
+                //debugger;
+                if (!seenJokes.has(jokeObj.id)) {
+                    seenJokes.add(jokeObj.id);
+                    j.push({ ...jokeObj, votes: 0 });
+                } else {
+                    console.error("duplicate found!");
+                }
+            }
+            this.setState({jokes: j})
+        } catch (e) {
+            console.log(e);
+        }
+    }
 
-  /* empty joke list and then call getJokes */
+    generateNewJokes() {
+        this.setState({jokes: []});
+        this.getJokes();
+    }
 
-  function generateNewJokes() {
-    setJokes([]);
-  }
+    vote(id, delta) {
+        this.setState(state => ({
+            jokes: state.jokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j))
+        })
+        );
+    }
 
-  /* change vote for this id by delta (+1 or -1) */
+    render() {
+        const {jokes} = this.state;
+        if (jokes.length) {
+            let sortedJokes = [...jokes].sort((a, b) => b.votes - a.votes);
+        
 
-  function vote(id, delta) {
-    setJokes(allJokes =>
-      allJokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j))
-    );
-  }
-
-  /* render: either loading spinner or list of sorted jokes. */
-
-  if (jokes.length) {
-    let sortedJokes = [...jokes].sort((a, b) => b.votes - a.votes);
-  
-    return (
-      <div className="JokeList">
-        <button className="JokeList-getmore" onClick={generateNewJokes}>
-          Get New Jokes
-        </button>
-  
-        {sortedJokes.map(j => (
-          <Joke text={j.joke} key={j.id} id={j.id} votes={j.votes} vote={vote} />
-        ))}
-      </div>
-    );
-  }
-
-  return null;
+            return (
+                <div className="JokeList">
+                    <button className="JokeList-getmore" onClick={this.generateNewJokes}>
+                        Get New Jokes
+                    </button>
+                    {sortedJokes.map(j => (
+                        <Joke
+                            text={j.joke}
+                            key={j.id}
+                            id={j.id}
+                            votes={j.votes}
+                            vote={this.vote}
+                        />
+                    ))}
+                </div>
+            );
+        }
+        return null;
+    }
 
 }
 
